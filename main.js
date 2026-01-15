3858417f61a86f346aa071a7577082f7891a7460
Ling Dev <98570765+lingneisandev@users.noreply.github.com>
2026-01-15 22:49:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /**
- * Ultimate Music Bot 
+ * Zeta Music Prime 
  * Comprehensive Discord Bot
  * 
  * @fileoverview Core application
  * @version 1.0.0
- * @author GlaceYT
+ * @author Lingneisan
  */
 
 const DiscordClientFramework = require('discord.js').Client;
@@ -374,4 +374,5 @@ enterpriseApplicationManager.executeApplicationBootstrap();
 
 
 module.exports = enterpriseApplicationManager.clientRuntimeInstance;
+
 shiva.initialize(enterpriseApplicationManager.clientRuntimeInstance);
